9b778f80dfdd054d0b844f7b69f56ca444f33610
Matteo Gheza <matteo.gheza07@gmail.com>
2022-02-07 00:46:51 +0100

    Test Svelte dashboard

---
+++ dashboard/src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+import { Styles } from 'sveltestrap';
+
+//export let name;
+</script>
+
+<style>
+:global(body) {
+	padding: 0;
+}
+</style>
+<Styles />
+
+<header>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <i class="navbar-brand d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2"
+                    viewBox="0 0 24 24">
+                    <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+                    <circle cx="12" cy="13" r="4" />
+                </svg>
+                <strong>CasparCG template OBS bridge</strong>
+            </i>
+        </div>
+    </div>
+</header>
+
+<main>
+
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">key</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">values.templateName</h6>
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-lg btn-outline-success">Play</button>
+                                    <button type="button" class="btn btn-lg btn-outline-danger">Stop</button>
+                                </div>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-danger">Disconnect</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</main>
